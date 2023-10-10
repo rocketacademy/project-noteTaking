55a019cd5663e8b26b0f74e31cef00886e7827e9
FoongL <leung.foong@gmail.com>
2023-10-11 01:18:23 +0800

    added mainPage comments

--- src/page/mainPage/MainPage.js
+++ src/page/mainPage/MainPage.js
@@ -1,12 +1,19 @@
 import { useState } from "react";
+
+// importing the components we will be using
 import NoteDetail from "../../components/NoteDetail/NoteDetail";
 import NoteList from "../../components/NoteList/NoteList";
+
+// importing css
 import "./MainPage.css";
 
 function MainPage() {
+  // we have a few states we will use to control the page view, as they will both be used by child components, we will set it here so they both have access
   const [mode, setMode] = useState(null);
   const [noteId, setNoteId] = useState(null)
 
+
+  // we will create functions to update our state, while we do not use it here, we will pass it down through props so the child components will ahve the ability to edit these states
   const modeChange = (action) =>{
     setMode(action)
   }
