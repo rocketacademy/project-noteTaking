23f311e4e3846009951ce9d59f243cdba7d0291f
FoongL <leung.foong@gmail.com>
2023-10-11 01:21:37 +0800

    added comments for notes provider

--- src/reducer/notes.js
+++ src/reducer/notes.js
@@ -1,7 +1,15 @@
+// Importing all the "actions" we can use in this app, we define these in another file. This is just so we can reuse these easily in the reducer and the action creators below
 import { GET, FETCH, ADD, EDIT, DELETE } from "./notesActionTypes";
 
+// This is our contexts initial state, much like how we have an initial state in our useState hooks, this will be the one for our context which will be put together in the provider.
 export const initialState = [];
 
+
+
+// This is the reducer, these are the actions we will allow our reducer to use, these actions have to match the actions in which we import in line 2 above.
+// you can see that the reducer takes in state and action, the action will be an object created by our functions below called the action Creators
+
+// You will also note that this is the ONLY place in this repo that touches upon local Storage, this is what we call "encapsulation" and it allows us to write code easier through out the rest of the project.
 export function notesReducer(state, action) {
   switch (action.type) {
     case GET:
@@ -38,6 +46,16 @@ export function notesReducer(state, action) {
       return state;
   }
 }
+
+
+/**
+ * 
+ * ACTION CREATORS
+ * 
+ * These are function we will call, and all it does is it will return an object that first the exact 
+ * requirement we need as "action" in the reducer function above.
+ * 
+ */
 
 export function getAction() {
   return {
