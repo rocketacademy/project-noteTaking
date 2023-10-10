d937bdf787febc86dc5a3e5123ccf93b013ba967
FoongL <leung.foong@gmail.com>
2023-10-11 01:15:38 +0800

    added a few comments

--- src/components/NoteDetail/NoteDetail.js
+++ src/components/NoteDetail/NoteDetail.js
@@ -3,6 +3,7 @@ import "./NoteDetail.css";
 import { NotesContext } from "../../provider/NotesProvider";
 import { addAction, deleteAction, editAction } from "../../reducer/notes";
 
+// this component can be used to viewing, writting or editting notes
 export default function NoteDetail(PROPS) {
   const { NotesDispatch, notesList } = useContext(NotesContext);
 
--- src/components/NoteList/NoteList.js
+++ src/components/NoteList/NoteList.js
@@ -3,6 +3,7 @@ import { useEffect, useContext } from "react";
 import { NotesContext } from "../../provider/NotesProvider";
 import { fetchAction } from "../../reducer/notes";
 
+// this component can be used to list out all existing notes and make them clickable to be shown in the noteDetail compenents
 export default function NoteList({modeChange, noteChange}) {
   const { NotesDispatch, notesList } = useContext(NotesContext);
 
